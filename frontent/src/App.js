b9499d8f1eac8c7135c974a2a225c39059ef3b75
Rajendra199 <rajendrasahoo1711@gmail.com>
2022-05-05 02:15:03 +0530

    second-commit

--- frontent/src/App.js
+++ frontent/src/App.js
@@ -1,11 +1,36 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import Home from './screens/Home';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <a href="/">amazon</a>
-      </header>
-      <main>list product</main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazon</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer className="text-center">
+          <div>All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
